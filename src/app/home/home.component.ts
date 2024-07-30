53e28f8850a344180bef5f6e51360fb7c78a0a79
Hugimo-kun <hugo.simoneau@gmail.com>
2024-07-30 15:25:49 +0200

    login form added

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  IDENTIFIANTS = {
+    email: 'admin@gmail.com',
+    password: 'motdepasse1234',
+  };
+  isVisible: boolean = false;
+
+  constructor(private router: Router) {}
+
+  public form: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    let formLogin = {
+      email: this.form.get('email')?.value,
+      password: this.form.get('password')?.value,
+    };
+
+    if (
+      formLogin.email === this.IDENTIFIANTS.email &&
+      formLogin.password === this.IDENTIFIANTS.password
+    ) {
+      this.isVisible = false;
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.isVisible = true;
+    }
+  }
+}
